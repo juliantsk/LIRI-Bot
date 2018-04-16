2e14460372a995009a7844b6a61fc50bf9deecb4
Julian T Skodzinski <juliantsk@gmail.com>
2018-04-16 17:00:54 -0400

    Finished functionality to access spotify and twitter. Established a function to read random requests from random.txt.

--- keys.js
+++ keys.js
@@ -1,13 +1,16 @@
-console.log('this is loaded');
+// console.log('this is loaded');
+require("dotenv").config();
 
-exports.twitter = {
+var twitter = {
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
-exports.spotify = {
+var spotify = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
 };
+
+module.exports = { twitter, spotify };
--- liri.js
+++ liri.js
@@ -1,10 +1,18 @@
-require("dotenv").config();
+var keys = require("./keys");
+var callAPI = require("request");
+var fs = require("fs");
+var readline = require("readline");
+var Spotify = require("node-spotify-api");
+var Twitter = require("twitter");
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
-var callAPI = require("request");
 var request = process.argv[2];
-var prop = process.argv[3];
+var property = "";
+
+for (var i = 3; process.argv[i]; i++) {
+    property += process.argv[i] + "+";
+}
 
 function liri(request, prop) {
     switch (request) {
@@ -12,10 +20,10 @@ function liri(request, prop) {
             myTweets();
             break;
         case "spotify-this-song":
-            spotifyThis();
+            spotifyThis(prop);
             break;
         case "movie-this":
-            movieThis();
+            movieThis(prop);
             break;
         case "do-what-it-says":
             doRandom();
@@ -23,78 +31,93 @@ function liri(request, prop) {
     };
 }
 
-liri(request, prop);
+liri(request, property);
 
-function spotifyThis() {
+function spotifyThis(prop) {
     // 'spotify-this-song'
-    // Show information about song:
-    // Artist(s), song's name, 
-    // preview link of the song,
-    // album that the song is from
-
+    var song = prop;
     // If no song provided,
-
+    if (song === "") song = "the sign by ace of base";
     // default to "The Sign" by Ace of Base.
+    // Show information about song:
+    spotify.search({ type: 'track', query: song }, function(err, data) {
+        if (err) console.log(err);
+        // Artist(s),
+        console.log("\n" + data.tracks.items[0].artists[0].name);
+        // song's name, 
+        console.log(data.tracks.items[0].name);
+        // preview link of the song,
+        console.log(data.tracks.items[0].external_urls.spotify);
+        // album that the song is from
+        console.log(data.tracks.items[0].album.name + "\n");
+    });
+
 }
 
 function myTweets() {
     // 'my-tweets'
     // Show your last 20 tweets and
+    var params = { screen_name: "nodejs" };
+    client.get("statuses/user_timeline", params, function(error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                console.log("\ntweet " + (i + 1) + ": " + tweets[i].text + "\n::::::: " + tweets[i].created_at + "\n");
+            }
+        }
+    });
     // when they were created
 
 }
 
-function movieThis() {
+function movieThis(prop) {
     // 'movie-this'
     //  Show: title, year, IMDB rating,
     // Rotten Tomatoes Rating, Country,
     // language, plot, actors.
-    var movieName = "";
-    // ...
-    for (var i = 2; process.argv[i]; i++) {
-        movieName += "+" + process.argv[i];
-    }
-
+    var movieName = prop;
+    // If no movie provided, default to "Mr. Nobody"
+    if (movieName === "") movieName = "Mr.Nobody";
 
     // Then run a request to the OMDB API with the movie specified
     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-
-    // This line is just to help us debug against the actual URL.
-    console.log(queryUrl);
-
-
     // Then create a request to the queryUrl
-    // ...
-    request(queryUrl, function(error, response, body) {
+    callAPI(queryUrl, function(error, response, body) {
 
         // If the request is successful
-        // ...
         if (!error && response.statusCode === 200) {
 
-            // Then log the Release Year for the movie
-            // ...
-            console.log("Title: " + JSON.parse(body).Title);
+            // Then log the relevant information...
+            console.log("\nTitle: " + JSON.parse(body).Title);
             console.log("Relase Year: " + JSON.parse(body).Year);
             console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings[1].Value);
             console.log("Country: " + JSON.parse(body).Year);
             console.log("Language: " + JSON.parse(body).Year);
             console.log("Plot: " + JSON.parse(body).Year);
-            console.log("Actors: " + JSON.parse(body).Year);
+            console.log("Actors: " + JSON.parse(body).Year + "\n");
         }
     });
-    // If no movie provided,
-
-    // default to "Mr. Nobody"
 }
 
 function doRandom() {
-    var tempRequest;
-    var tempProp;
+    var tempRequest = "";
+    var tempProperty = "";
     // 'do-what-it-says'
     // Take the text in random.txt...
-    // ...read a random line
+    fs.readFile("./random.txt", "utf8", function(err, data) {
+        if (err) throw err;
+        // Split the file into lines...
+        var lines = data.split("\r\n");
+        // ...pick a random number...
+        var random = Math.floor(Math.random() * Math.floor(lines.length));
+        // ...read a random line, and split it into an array with two strings...
+        var properties = lines[random].split(",", 2);
+        // ...assign the two strings to the two temporary variables...
+        tempRequest = properties[0];
+        tempProperty = properties[1];
+        // ...run liri with the updated variables.
+        liri(tempRequest, tempProperty);
+    });
 
-    // ...and call one of LIRI's commands.
-    liri(tempRequest, tempProp);
+
 }
